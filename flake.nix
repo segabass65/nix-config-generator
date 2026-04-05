810c02065e11f4ae48dac428091fd9d1e9c5ecc0
segabass65 <segabass65@proton.me>
2026-04-05 16:43:50 +0300

    feat: add extraConfig for toSxhkd func

--- flake.nix
+++ flake.nix
@@ -1,16 +1,22 @@
 {
-  outputs = { self, ... }: {
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  
+  outputs = { self, nixpkgs, ... }: let 
+    inherit (nixpkgs) lib;
+
+  in {
     lib = {
-      toSxhkdKeybindings = { pkgs, keybindings }:
-        pkgs.lib.concatStringsSep "\n" (
-          pkgs.lib.mapAttrsToList (
-            hotkey: command:
-              pkgs.lib.optionalString (command != null) ''
-                ${hotkey}
-                  ${command}
-              ''
-          ) keybindings
-        );
+      toSxhkd = { keybindings, extraConfig ? "" }: let
+        config = lib.mapAttrsToList (
+          hotkey: command:
+            lib.optionalString (command != null) ''
+              ${hotkey}
+                ${command}
+            ''
+        ) keybindings;
+
+      in
+        lib.concatStringsSep "\n" (config ++ [ extraConfig ]);
     };
   };
 }
