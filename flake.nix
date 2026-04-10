03df68ca0ac4b958913e877eca553d50cf40d542
segabass65 <segabass65@proton.me>
2026-04-10 17:24:22 +0300

    feat: add toPolybar func

--- flake.nix
+++ flake.nix
@@ -7,7 +7,7 @@
   in {
     lib = {
       toSxhkd = { keybindings, extraConfig ? "" }: let
-        keybindingsList = lib.mapAttrsToList (
+        keybindings' = lib.mapAttrsToList (
           hotkey: command:
             lib.optionalString (command != null) ''
               ${hotkey}
@@ -16,7 +16,85 @@
         ) keybindings;
 
       in
-        lib.concatStringsSep "\n" (keybindingsList ++ [ extraConfig ]);
+        lib.concatStringsSep "\n" (keybindings' ++ [ extraConfig ]);
+
+      toPolybar = { config ? {}, settings ? {}, extraConfig ? "" }: let
+
+        # Converts an attrset to INI text, quoting values as expected by polybar.
+        # This does no more fancy conversion.  
+        toPolybarIni = lib.generators.toINI {
+          mkKeyValue = key: value: let
+            value' = if lib.isBool value then
+              if value then "true" else "false"
+
+            else if 
+              lib.isString value &&
+              key != "include-file" &&
+              (lib.hasPrefix " " value || lib.hasSuffix " " value)
+
+            then
+              ''"${value}"''
+            else
+              toString value;
+
+          in
+            "${key}=${value'}";
+        };
+
+        # Convert a key/value pair to the insane format that polybar uses.
+        # Each input key/value pair may return several output key/value pairs.
+        convertPolybarKeyVal = key: value:
+
+          # Convert { foo = [ "a" "b" ]; }
+          # to {
+          #   foo-0 = "a";
+          #   foo-1 = "b";
+          # }
+          if lib.isList value then
+            lib.concatLists (
+              lib.imap0 (i: convertPolybarKeyVal "${key}-${toString i}")
+              value
+            )
+
+          # Convert {
+          #   foo.text = "a";
+          #   foo.font = 1;
+          # } to {
+          #   foo = "a";
+          #   foo-font = 1;
+          # }
+          else if lib.isAttrs value && !lib.isDerivation value then
+            lib.concatLists (
+              lib.mapAttrsToList (
+                k: convertPolybarKeyVal (
+                  if k == "text" then key else "${key}-${k}"
+                )
+              )
+              value
+            )
+
+          # Base case
+          else
+            [ (lib.nameValuePair key value) ];
+        
+        convertPolybarSection = _: attrs:
+          lib.listToAttrs (
+            lib.concatLists (lib.mapAttrsToList convertPolybarKeyVal attrs)
+          );
+
+      in if
+        config != {} ||
+        settings != {} ||
+        extraConfig != ""
+
+      then ''
+        ${toPolybarIni config}
+        ${toPolybarIni (lib.mapAttrs convertPolybarSection settings)}
+        ${extraConfig}
+      ''
+
+      else
+        null;
     };
   };
 }
